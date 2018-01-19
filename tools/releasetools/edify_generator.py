24dd03f83bff9821a430fd9086162e134d287ef1
edi194 <misiakadrian@gmail.com>
2018-01-19 10:44:24 +0100

    Magisk integration 1/2

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -166,6 +166,11 @@ class EdifyGenerator(object):
 
   def RunBackup(self, command):
     self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s");' % command))
+	
+  def FlashMagisk(self):
+    self.script.append('package_extract_dir("magisk", "/tmp/magisk");')
+    self.script.append('run_program("/sbin/busybox", "unzip", "/tmp/magisk/magisk.zip", "META-INF/com/google/android/*", "-d", "/tmp/magisk");')
+    self.script.append('run_program("/sbin/sh", "/tmp/magisk/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/magisk/magisk.zip");')
 
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
